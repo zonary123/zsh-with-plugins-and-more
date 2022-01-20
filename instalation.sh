b201bc039bc5e8d935e8039cbc6b9f412b649a0b
zonary123 <carlosvarasalonso10@gmail.com>
2022-01-20 23:24:50 +0100

    ss

--- instalation.sh
+++ instalation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Instalacion de zsh
-apt install zsh 
+sudo apt install zsh 
 
 #Instalacion de lsd y bat
 wget https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb
