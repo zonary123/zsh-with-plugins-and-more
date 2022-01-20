385d91dcd97baec121183723abbf4600f3f1360b
zonary123 <carlosvarasalonso10@gmail.com>
2022-01-20 22:48:26 +0100

    menos tamaño

--- instalation.sh
+++ instalation.sh
@@ -3,10 +3,13 @@
 apt install zsh 
 
 #Instalacion de lsd y bat
-sudo dpkg -i ./cosas/lsd_0.21.0_amd64.deb ./cosas/bat_0.19.0_amd64.deb
+wget https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb
+wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
+sudo dpkg -i ./lsd_0.21.0_amd64.deb ./bat_0.19.0_amd64.deb
 
 #Instalacion de la tipografia Hack nerd Fonts
-mv ./fonts/Hack.zip /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip 
+mv ./Hack.zip /usr/share/fonts
 unzip /usr/share/fonts/Hack.zip
 
 #Creacion de la carpeta zsh-p10k y copia de las carpetas a sus respectivos directorios
