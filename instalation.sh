19fd48756e748618342f0bd1b15f630e1bf66fec
zonary123 <carlosvarasalonso10@gmail.com>
2022-01-21 19:54:53 +0100

    Fix plugins zsh

--- instalation.sh
+++ instalation.sh
@@ -9,13 +9,19 @@ sudo dpkg -i ./lsd_0.21.0_amd64.deb ./bat_0.19.0_amd64.deb
 
 #Instalacion de la tipografia Hack nerd Fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip 
-mv ./Hack.zip /usr/share/fonts
-unzip /usr/share/fonts/Hack.zip
+sudo mv ./Hack.zip /usr/share/fonts
+sudo unzip /usr/share/fonts/Hack.zip
 
 #Creacion de la carpeta zsh-p10k y copia de las carpetas a sus respectivos directorios
 sudo mkdir /usr/share/zsh-p10k 
 sudo cp -r ./powerlevel10k /usr/share/zsh-p10k
 sudo cp -r ./{.p10k.zsh,.zshrc} /etc/skel
+sudo cp -r ./{.p10k.zsh,.zshrc} $HOME
+sudo cp -r ./{.p10k.zsh,.zshrc} /root
+
+#Plugins ZSH
+git clone https://github.com/zsh-users/zsh-autosuggestions ./plugins-zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ./plugins-zsh/zsh-syntax-highlighting
 sudo cp -r plugins-zsh /usr/share
 
 #Cambio de la terminal del usuario a zsh
@@ -25,3 +31,6 @@ sudo usermod --shell /usr/bin/zsh $(whoami)
 #Instalacion de fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+#Cambio a la nueva terminal
+zsh
