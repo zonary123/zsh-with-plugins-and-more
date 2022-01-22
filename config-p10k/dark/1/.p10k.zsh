349196faa9920fea15869a19acef241ed44849e4
Zonary123 <carlosvarasalonso10@gmail.com>
2022-01-22 01:55:40 +0100

    a

--- config-p10k/dark/1/.p10k.zsh
+++ config-p10k/dark/1/.p10k.zsh
@@ -191,9 +191,9 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=81
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=255
   # Custom icon.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
+  #typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION=''
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
