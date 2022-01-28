3b593c74a04bb7815c76f1e39c3e8f797fbdabfb
Zonary123 <carlosvarasalonso10@gmail.com>
2022-01-28 01:24:20 +0100

    Fix copydir plugin

--- plugins-zsh/zsh-copydir/copydir.plugin.zsh
+++ plugins-zsh/zsh-copydir/copydir.plugin.zsh
@@ -1,5 +1,11 @@
 # Copies the pathname of the current directory to the system or X Windows clipboard
 function copydir {
   emulate -L zsh
+  echo $PWD > $HOME/pwd.txt
+  xclip -i $HOME/pwd.txt
   print -n $PWD | clipcopy
+}
+function cdcopydir()
+{
+  cd $(xclip -o)
 }
