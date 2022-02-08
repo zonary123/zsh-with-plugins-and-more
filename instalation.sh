f25f47ae83afcfc0fd67f0b351c092cf50dc11ac
zonary123 <carlosvarasalonso10@gmail.com>
2022-02-08 22:44:20 +0100

    fix

--- instalation.sh
+++ instalation.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+#========================================#
+#               Colores                  #
+#========================================#
 negro="\e[0;30m"
-gris-oscuro="\e[0;31m"
+grisoscuro="\e[0;31m"
+azul="\e[0;34m"
+azulreslatado="\e[1;31m"
+verde="\e[0;32m"
+verderesaltado="\e[0;32m"
 cian="\e[0;36m"
+cianresaltado="\e[1;36m"
 rojo="\e[0;31m"
+rojoresaltado="\e[1;31m"
+purpura="\e[0;35m"
+purpuraresaltado="\e[1;35m"
+cafe="\e[0;33m"
 amarillo="\e[1;33m"
+gris="\e[0;37m"
+blanco="\e[0;31m"
 
+#========================================#
+#                Start                   #
+#========================================#
 echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Instalacion de paquetes"
 
 
--- plugins-zsh/aliases.plugin.zsh
+++ plugins-zsh/aliases.plugin.zsh
@@ -4,3 +4,4 @@ alias -g T='| tail'
 alias -g G='| grep'
 alias -g L="| less"
 alias -g M="| most"
+alias ip="ip --color"
