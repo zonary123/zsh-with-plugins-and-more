1ea474620d33bb3144ae84145c1aed6e29741ed9
zonary123 <carlosvarasalonso10@gmail.com>
2022-02-09 05:27:55 +0100

    ´8

--- instalation.sh
+++ instalation.sh
@@ -45,13 +45,13 @@ git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
 sudo chmod +x /home/carlos/git/clone/zsh-with-plugins-and-more/polybar-themes/setup.sh
 /home/carlos/git/clone/zsh-with-plugins-and-more/polybar-themes/setup.sh
 
-sudo cp -rf ./config/bspwm /home/carlos/.config
-sudo cp -rf ./config/sxhkd /home/carlos/.config
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/bspwm /home/carlos/.config
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/sxhkd /home/carlos/.config
 sudo rm -r /home/carlos/.wallpapers/wallpaper.jpg
 sudo rm -r /home/carlos/.config/polybar/shapes
-sudo cp -rf ./config/.wallpapers /home/carlos
-sudo cp -rf ./config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
-sudo cp -rf ./config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/.wallpapers /home/carlos
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
 
 #Picom
 sudo mkdir /home/carlos/.config/picom
@@ -69,10 +69,10 @@ echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Instalacion de bat lsd y descar
 wget https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb
 wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip 
-sudo dpkg -i ./lsd_0.21.0_amd64.deb
-sudo dpkg -i ./bat_0.19.0_amd64.deb
-sudo rm ./lsd_0.21.0_amd64.deb
-sudo rm ./bat_0.19.0_amd64.deb
+sudo dpkg -i /home/carlos/git/clone/zsh-with-plugins-and-more/lsd_0.21.0_amd64.deb
+sudo dpkg -i /home/carlos/git/clone/zsh-with-plugins-and-more/bat_0.19.0_amd64.deb
+sudo rm /home/carlos/git/clone/zsh-with-plugins-and-more/lsd_0.21.0_amd64.deb
+sudo rm /home/carlos/git/clone/zsh-with-plugins-and-more/bat_0.19.0_amd64.deb
 #Comandos Git clone
 
 echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Clonacion de repositorios github powerlevel10k | plugins de zsh => zsh-autosuggestions y zsh-syntax-highlighting"
@@ -81,18 +81,18 @@ git clone https://github.com/zsh-users/zsh-autosuggestions /home/carlos/git/clon
 git clone https://github.com/zsh-users/zsh-syntax-highlighting /home/carlos/git/clone/zsh-with-plugins-and-more/plugins-zsh/zsh-syntax-highlighting
 
 #Instalacion de la tipografia Hack nerd Fonts
-sudo mv ./Hack.zip /usr/share/fonts
+sudo mv /home/carlos/git/clone/zsh-with-plugins-and-more/Hack.zip /usr/share/fonts
 
     echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Moviendo archivos .p10k.zsh y .zshrc al directorio personal del usuario directorio de root y a al directorio /etc/skel"
 mkdir /usr/share/zsh-p10k 
 sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/powerlevel10k /usr/share/zsh-p10k
 sudo sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/{.p10k.zsh,.zshrc} /etc/skel
-sudo sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/{.p10k.zsh,.zshrc} $HOME
+sudo sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/{.p10k.zsh,.zshrc} /home/carlos
 sudo sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/{.p10k.zsh,.zshrc} /root
 
 echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Instalacion de los plugins"
 #Plugins ZSH
-sudo cp -rf ./plugins-zsh /usr/share
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/plugins-zsh /usr/share
 
 echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Cambio de terminal a los perfiles $USERNAME y root"
 #Cambio de la tesudo rminal del usuario a zsh
@@ -109,10 +109,10 @@ sudo cp -rf /home/carlos/.fzf /root
 
 sudo ln -s /home/carlos/.zshrc /root/.zshrc 
 sudo ln -s /home/carlos/.p10k.zsh /root/.p10k.zsh 
-/home/carlos/git/clone/polybar-themes/setup.sh
-sudo cp -rf ./config/sxhkd /home/carlos/.config
-sudo cp -rf ./config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
-sudo cp -rf ./config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
+/home/carlos/git/clone/zsh-with-plugins-and-more/polybar-themes/setup.sh
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/sxhkd /home/carlos/.config
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
+sudo cp -rf /home/carlos/git/clone/zsh-with-plugins-and-more/config/polybar/polybar-themes/simple/shapes /home/carlos/.config/polybar
 sudo chmod -R 777 /home/carlos/.config/polybar
 #/etc/skel
 sudo cp -rf /home/carlos/.fzf /etc/skel
@@ -122,8 +122,10 @@ echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Configuracion del zsh a partir 
 read -rp "Quieres una configuracion determinada si/no" respuesta
 if [ "$respuesta" == "si" ]
 then
-    ./Themes.sh
+    /home/carlos/git/clone/zsh-with-plugins-and-more/Themes.sh
 else
     p10k configure
 fi
 sudo apt update && sudo apt upgrade
+cd /usr/share/fonts
+sudo unzip /usr/share/fonts/Hack.zip
