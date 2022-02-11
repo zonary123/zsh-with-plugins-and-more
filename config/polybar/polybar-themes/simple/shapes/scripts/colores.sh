fbe39bfb2a7721e609174231800013a666bed478
zonary123 <carlosvarasalonso10@gmail.com>
2022-02-11 02:21:36 +0100

    script colores.sh
    script para la polybar para colores en la polybar a X horas

--- config/polybar/polybar-themes/simple/shapes/scripts/colores.sh
+++ config/polybar/polybar-themes/simple/shapes/scripts/colores.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 hora=$(date | cut -d " " -f 5 | cut -d ":" -f 1)
-if (( "0","1","2","3","4","5" == "$hora" ))
+if (( $hora < 6 ))
 then
-~/git/clone/polybar-themes/simple/shapes/scripts/colors-dark.sh --blue-gray
-elif (( "6","7","8","9","10","11" == "$hora" ))
+~/.config/polybar/shapes/scripts/colors-dark.sh --blue-gray
+elif (( $hora < 12 ))
 then
-~/git/clone/polybar-themes/simple/shapes/scripts/colors-dark.sh --purple
-elif (( "12","13","14","15","16","17" == "$hora" ))
+~/.config/polybar/shapes/scripts/colors-dark.sh --purple
+elif (( $hora < 19 ))
 then
-~/git/clone/polybar-themes/simple/shapes/scripts/colors-dark.sh --blue
-elif (( "18","19","20","21","22","23" == "$hora" ))
+~/.config/polybar/shapes/scripts/colors-dark.sh --blue
+elif (( $hora < 25 ))
 then
-~/git/clone/polybar-themes/simple/shapes/scripts/colors-dark.sh --gray
-fi 
+~/.config/polybar/shapes/scripts/colors-dark.sh --gray
+fi
