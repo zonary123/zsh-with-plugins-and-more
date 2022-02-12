d0720447f9c8f025a59a11911cdd426b28fef427
zonary123 <carlosvarasalonso10@gmail.com>
2022-02-12 06:02:00 +0100

    more wallpapers

--- config/script/Imagenes.sh
+++ config/script/Imagenes.sh
@@ -15,7 +15,7 @@ then
     wget https://images.hdqwalls.com/download/ark-knight-girl-anime-4k-by-1920x1080.jpg ~/.wallpapers/anime
     wget https://wallpaperaccess.com/full/3177905.jpg ~/.wallpapers/anime
     wget https://wallpapercave.com/wp/wp5420118.jpg ~/.wallpapers/anime
-    wget ~/.wallpapers/anime
+    wget https://www.nawpic.com/media/2020/naruto-4k-nawpic.png ~/.wallpapers/anime
     wget ~/.wallpapers/anime
     wget ~/.wallpapers/anime
     wget ~/.wallpapers/anime
@@ -39,6 +39,33 @@ fi
 read -p "Desea instalar wallpapers de fondo de pantalla" wallpapers
 if [[ $wallpapers == "wallpapers" ]]    # ← see 'man bash' for valid conditional statements.
 then
+    wget https://images5.alphacoders.com/958/thumb-1920-958580.jpg ~/.wallpapers/wallapapers
+    wget https://p4.wallpaperbetter.com/wallpaper/468/838/392/4k-artistic-5k-colors-wallpaper-thumb.jpg ~/.wallpapers/wallapapers
+    wget https://images4.alphacoders.com/932/thumb-1920-932271.jpg ~/.wallpapers/wallapapers
+    wget https://c4.wallpaperflare.com/wallpaper/974/565/254/windows-11-windows-10-minimalism-hd-wallpaper-preview.jpg ~/.wallpapers/wallapapers
+    wget https://wallpaperaccess.com/full/2159510.jpg ~/.wallpapers/wallapapers
+    wget https://wow.zamimg.com/uploads/blog/images/20591-afterlives-revendreth-4k-desktop-wallpapers.jpg ~/.wallpapers/wallapapers
+    wget https://i.blogs.es/cfa482/illusion_1440x900/450_1000.jpg ~/.wallpapers/wallapapers
+    wget https://i.pinimg.com/originals/5d/01/c7/5d01c74da34ec298f5d38544e6bd5e16.png ~/.wallpapers/wallapapers
+    wget https://cdn.wallpapersafari.com/15/96/bJUmx3.jpg ~/.wallpapers/wallapapers
+    wget https://i.pinimg.com/originals/68/54/d0/6854d0f73ccbe67de1333d3eaad37d64.jpg ~/.wallpapers/wallapapers
+    wget https://p4.wallpaperbetter.com/wallpaper/975/1021/255/asus-rog-neon-logo-4k-wallpaper-preview.jpg ~/.wallpapers/wallapapers
+    wget https://www.xtrafondos.com/descargar.php?id=4414&resolucion=3840x2160 ~/.wallpapers/wallapapers
+    wget https://i.pinimg.com/originals/4f/50/38/4f503844d9a44b0350c25eeefae028d3.jpg ~/.wallpapers/wallapapers
+    wget https://www.xtrafondos.com/thumbs/1_3635.jpg ~/.wallpapers/wallapapers
+    wget https://amusd.net/wp-content/uploads/2021/03/HD-4K-Wallpaper.jpg ~/.wallpapers/wallapapers
+    wget https://cdnb.artstation.com/p/marketplace/presentation_assets/000/341/707/large/file.jpg?1586770665 ~/.wallpapers/wallapapers
+    wget https://free4kwallpapers.com/uploads/originals/2020/11/26/thor-stormbreaker-minimalist-wallpaper.jpg ~/.wallpapers/wallapapers
+    wget https://coatepec.gob.mx/wp-content/uploads/2019/09/4K-Moving-Stars-Live-Wallpaper-1.jpg ~/.wallpapers/wallapapers
+    wget https://cdn.wallpapersafari.com/15/73/WbFGQ1.jpg ~/.wallpapers/wallapapers
+    wget https://cutewallpaper.org/21/ultra-hd-space-wallpaper/hd-space-wallpapers-planet-horizont-space-4k-ultra-hd-.jpg ~/.wallpapers/wallapapers
+    wget https://cdn.statically.io/img/fancyodds.com/wp-content/uploads/2021/12/Rgb-Wallpaper-Live-4k-HD-For-Desktop-Iphone-Android-Laptop-4.jpg ~/.wallpapers/wallapapers
+    wget https://wow.zamimg.com/uploads/screenshots/normal/980483.jpg ~/.wallpapers/wallapapers
+    wget https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/4k/original/07.jpg ~/.wallpapers/wallapapers
+    wget https://c4.wallpaperflare.com/wallpaper/860/945/126/romantic-couple-4k-pics-ultra-hd-wallpaper-preview.jpg ~/.wallpapers/wallapapers
+    wget https://c4.wallpaperflare.com/wallpaper/184/515/626/digital-digital-art-artwork-illustration-drawing-hd-wallpaper-preview.jpg ~/.wallpapers/wallapapers
+    wget ~/.wallpapers/wallapapers
+    wget ~/.wallpapers/wallapapers
     wget ~/.wallpapers/wallapapers
     clear
 fi
