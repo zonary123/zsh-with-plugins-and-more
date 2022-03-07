af33136c8ab793825e5d66b093ff356c5048f19b
zonary123 <carlosvarasalonso10@gmail.com>
2022-03-08 00:04:50 +0100

    a

--- instalation.sh
+++ instalation.sh
@@ -69,15 +69,16 @@ clear
 
 #Comandos wget
 echo -e "${amarillo}[${rojo}*${amarillo}]${cian} Instalacion de bat lsd y descarga de la fuente de hack nerd fonts"
-wget https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb
-wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip 
-wget https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb
-sudo dpkg -i vivid_0.8.0_amd64.deb
-sudo dpkg -i ~/git/clone/zsh-with-plugins-and-more/lsd_0.21.0_amd64.deb
-sudo dpkg -i ~/git/clone/zsh-with-plugins-and-more/bat_0.19.0_amd64.deb
-sudo rm ~/git/clone/zsh-with-plugins-and-more/lsd_0.21.0_amd64.deb
-sudo rm ~/git/clone/zsh-with-plugins-and-more/bat_0.19.0_amd64.deb
+sudo wget https://github.com/sharkdp/bat/releases/download/v0.19.0/bat_0.19.0_amd64.deb
+sudo wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip 
+sudo wget https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb
+sudo dpkg -i ./vivid_0.8.0_amd64.deb
+sudo dpkg -i ./lsd_0.21.0_amd64.deb
+sudo dpkg -i ./bat_0.19.0_amd64.deb
+sudo rm ./lsd_0.21.0_amd64.deb
+sudo rm ./bat_0.19.0_amd64.deb
+sudo rm ./vivid_0.8.0_amd64.deb
 #Instalacion de colores de lsd
 sudo chmod -R +x /usr/share/lsd
 sudo mkdir -p /usr/share/lsd
